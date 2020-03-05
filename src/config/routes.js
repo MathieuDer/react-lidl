ec818cb0207c217a97029b96cec54210bbdc4b8f
mathieu derouiche <mathieu.derouiche@ynov.com>
2020-03-05 12:54:40 +0100

    Update router to display nav only on protected routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import Login from '../screens/login'
 import PrivateRoute from './privateRoute'
 import ProductsScreen from '../screens/productsScreen'
@@ -9,17 +14,30 @@ import HeaderComponent from '../components/header/HeaderComponent'
 const Routes = () => {
   return (
     <Router>
-      <HeaderComponent />
       <Switch>
-        <Route exact path='/' component={Login}></Route>
-        <PrivateRoute
-          exact
-          path='/products'
-          component={ProductsScreen}
-        ></PrivateRoute>
+        <Route exact path='/' component={LoginContainer} />
+        <Route component={DefaultContainer} />
       </Switch>
     </Router>
   )
 }
+
+const LoginContainer = () => (
+  <div>
+    <Route exact path='/' render={() => <Redirect to='/' />} />
+    <Route path='/' component={Login} />
+  </div>
+)
+
+const DefaultContainer = () => (
+  <div>
+    <HeaderComponent />
+    <PrivateRoute
+      exact
+      path='/products'
+      component={ProductsScreen}
+    ></PrivateRoute>
+  </div>
+)
 
 export default Routes
